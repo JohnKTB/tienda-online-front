98a450b6c22980d43694dd4f462c29fd27dd8567
johnKTB <johnrenato_8@hotmail.com>
2021-05-16 22:21:36 -0500

    agregando nueva url api

--- statics/js/api.js
+++ statics/js/api.js
@@ -1,18 +1,18 @@
 export default class API {
   async getProducts() {
-    const data = await fetch(`http://127.0.0.1:8000/api/products/`);
+    const data = await fetch(`http://167.99.117.129/api/products/`);
     const response = await data.json();
     return response;
   };
   async filterProducts(product) {
     const data = await fetch(
-      `http://127.0.0.1:8000/api/filter-product/${product}`
+      `http://167.99.117.129/api/filter-product/${product}`
     ); 
     const response = await data.json();
     return response;
   };
   async getCategories() {
-    const data = await fetch(`http://127.0.0.1:8000/api/categories/`);
+    const data = await fetch(`http://167.99.117.129/api/categories/`);
     const response = await data.json();
     return response;
   };
